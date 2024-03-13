2dae288a29668119376966a7e1d1994ddaf812b8
Ethan Chan <eton1234@outlook.com>
2024-03-12 22:29:36 -0500

    added led_matrix.h file

--- software/apps/led_matrix/led_matrix.h
+++ software/apps/led_matrix/led_matrix.h
@@ -7,5 +7,5 @@
 // Initialize the LED matrix display
 void led_matrix_init(void);
 void readText(char* text); 
-
+void updateLED(int32_t letterIndex);    
 // You may need to add more functions here
